49cef4873fa5cf22c398add40806a85a5686138c
NicolasLaime <nicolaime100@gmail..com>
2025-07-04 17:31:40 -0300

    Soporte CORS para integración frontend-backend en producción 3 backend

---
+++ backend/src/main/java/com/backend/foro/config/CorsGlobalConfig.java
@@ -0,0 +1,28 @@
+package com.backend.foro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://foro-patagonia.vercel.app"
+        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true); // importante si usás cookies o Authorization headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+}
