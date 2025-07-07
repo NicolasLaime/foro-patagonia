f723ae89d8a87133fcffc487960d4879ab65545f
NicolasLaime <nicolaime100@gmail..com>
2025-07-07 15:40:02 -0300

    Solucionamos error cors

--- backend/src/main/java/com/backend/foro/config/CorsGlobalConfig.java
+++ backend/src/main/java/com/backend/foro/config/CorsGlobalConfig.java
@@ -1,12 +1,14 @@
 package com.backend.foro.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
 public class CorsGlobalConfig {
 
     @Bean
